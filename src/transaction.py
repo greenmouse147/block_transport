f7359fe38e25cd190724daec24f9afd7177a90bf
greenmouse147 <cdaisi@axelfoulon.fr>
2018-02-05 20:38:32 +0100

    transaction part 1

--- src/transaction.py
+++ src/transaction.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+#codding : utf-8
 import hashlib 
 from time import strftime, gmtime 
 
@@ -20,29 +20,49 @@ class Transaction:
 
 	"""
 
-	def __init__(self, publicKey, clearMsg, EncMsg, event, code):
+	def __init__(self, publicKey, util, reputation, event, code,  privateKey, ):
 
-		eventTypes = {
+		codeTypes = {
 					1 : 'Alerte',
-					2 : 'Ajout de réponses',
+					2 : 'Ajout de réputation',
 					3 : 'Retrait de réputation',
 					4 : 'Création utilisation',
 					5 : 'Création entreprise',
 		}
-		codeTypes = {
+		eventTypes = {
 					1 : 'Accident',
 					2 : 'Vol',
 					3 : 'Agression',
 					4 : 'Incendie',
 					5 : 'Retard',
 		}
-		if event < 1 or event > 5:
-			raise EventType("Out of range event type (1-5), you specified {:d}".format(event))
 		if code < 1 or code > 5:
-			raise CodeType("Out of range code type (1-5), you specified {:d}".format(code))
-		self.publicKey = publicKey
-		self.transactionType = eventTypes[event]
-		self.codeType = codeTypes[code]
+			raise EventType("Out of range event type (1-5), you specified {:d}".format(code))
+		if event < 1 or event > 5:
+			raise CodeType("Out of range code type (1-5), you specified {:d}".format(event))
+
+		
+
+		#Detection type de trame
+		self.transactionType = codeTypes[event]
+		if self.transactionType == 4
+			new_user = user()
+			
+			self.util == None
+			self.reputation = 5
+			self.event = None
+			
+		else :
+			self.util = util
+			self.reputation = reputation
+			self.code = code 
+		
+
+
+		self.publicKey = publicKey		
+		self.privateKey = privateKey
+		
+		self.codeType = eventTypes[code]
 		self.time = strftime("%d %b %Y %H:%M:%S +0000", gmtime())
 		self.hashClearMsg = hashlib.sha256(clearMsg.encode('utf-8')).hexdigest()
 		self.hashEncMsg = hashlib.sha256(EncMsg.encode('utf-8')).hexdigest()
@@ -52,4 +72,3 @@ if __name__ == '__main__':
 
 	a = Transaction(clearMsg="This is a test", EncMsg="sqdknqsdoqsd", event=1, code=1)
 	print(a.time)
-
